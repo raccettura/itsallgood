5203a4344ccb3870bfb38a7a2ac272ab58d59f39
raccettura <raccettura@b6d84609-0aeb-3b61-2b96-96562cb4dc55>
2011-06-11 21:27:05 +0000

    Add support for Darwin/BSD OS's, more flexible response types per OS and set groundwork for supporting more OS's

--- modules/CPU_Load.php
+++ modules/CPU_Load.php
@@ -17,8 +17,8 @@ class Check_CPU_Load extends CheckBase {
     );
 
     public function check(){
-        $this->average = $this->get_cpu_avg();
-        if($this->average[1] > $this->config['maxCPU']){
+		$this->get_cpu_avg();
+        if($this->average['1m'] > $this->config['maxCPU'] || !isset($this->average['1m'])){
             return false;
         }
         return true;
@@ -28,20 +28,48 @@ class Check_CPU_Load extends CheckBase {
         if(!isset($this->average)){
             $this->check();
         }
-        return array(
-            '1m' => array('1 min avg', $this->average[0]),
-            '5m' => array('5 min avg', $this->average[1]),
-            '10m' => array('10 min avg', $this->average[2]),
-            'tproc' => array('run proc/total proc', $this->average[3]),
-            'procId' => array('last proc id', $this->average[4])
-        );
+        
+        if(!isset($this->average)){
+        	return "Unsupported OS";
+        }
+        
+		$responseTypes = Array(
+			'1m' => '1 min avg',
+			'5m' => '5 min avg',
+			'10m' => '10 min avg',
+			'tproc' => 'run proc/total proc',
+			'procId' => 'last proc id',
+		);
+
+		$responseArray = Array();
+		foreach($responseTypes as $key => $label){
+			if(isset($this->average[$key])){
+				$responseArray[$key] = array($label, $this->average[$key]);
+			}
+		}
+
+		return $responseArray;
     }
 
     private function get_cpu_avg(){
-        //XXX TODO find a Darwin/Windows way of doing this
-        $response_string = shell_exec('cat /proc/loadavg');
-        $response_array = explode(' ', $response_string);
-        return $response_array;
+		$osString = strtolower(php_uname());
+
+        // This means we're running some sort of Linux based OS
+        if(strpos($osString, 'linux') !== false){
+	        $response_string = shell_exec('cat /proc/loadavg');
+	        list($this->average['1m'], $this->average['5m'], $this->average['10m'], $this->average['tproc'], $this->average['procId']) = explode(' ', $response_string);
+			return true;
+        }
+        
+        // For BSD/Mac OS X
+        else if(strpos($osString, 'bsd') !== false || strpos($osString, 'darwin') !== false){
+	        $response_string = shell_exec('sysctl -n vm.loadavg');
+	        list($foo, $this->average['1m'], $this->average['5m'], $this->average['10m'], $foo) = explode(' ', $response_string);
+			return true;
+        }
+
+		// Nothing found
+		return false;
     }
 }
 ?>
